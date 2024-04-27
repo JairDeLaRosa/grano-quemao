08d4fb51365caefb33bb999bd4f05df1440d9acf
JairDeLaRosa <128413355+JairDeLaRosa@users.noreply.github.com>
2024-04-27 12:39:26 -0500

    gestion de actividades

---
+++ src/gestion-de-cultivos/AgregarActividad.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const AgregarActividad = () => {
+  return (
+      
+      <div className="editar">
+        <div className="containerForm">
+            Hola
+        </div>
+      </div>
+
+  );
+};
